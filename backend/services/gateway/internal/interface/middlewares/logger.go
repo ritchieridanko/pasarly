0c6379796a6051f86746d34d19d117dcb6d91c69
Ritchie Ridanko <ritchieridanko@gmail.com>
2025-11-28 06:14:36 +0700

    feat(gateway): implement error handling in Logger middleware

--- backend/services/gateway/internal/interface/middlewares/logger.go
+++ backend/services/gateway/internal/interface/middlewares/logger.go
@@ -1,12 +1,15 @@
 package middlewares
 
 import (
+	"errors"
 	"net/http"
 	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/ritchieridanko/pasarly/backend/services/gateway/internal/constants"
 	"github.com/ritchieridanko/pasarly/backend/services/gateway/internal/infra/logger"
+	"github.com/ritchieridanko/pasarly/backend/services/gateway/internal/utils"
+	"github.com/ritchieridanko/pasarly/backend/shared/ce"
 	"go.opentelemetry.io/otel/trace"
 	"go.uber.org/zap"
 )
@@ -18,7 +21,6 @@ func Logger(l *logger.Logger) gin.HandlerFunc {
 
 		requestID, _ := ctx.Request.Context().Value(constants.CtxKeyRequestID).(string)
 		traceID := trace.SpanFromContext(ctx.Request.Context()).SpanContext().TraceID().String()
-		status := ctx.Writer.Status()
 
 		fields := []zap.Field{
 			zap.String("request_id", requestID),
@@ -27,14 +29,41 @@ func Logger(l *logger.Logger) gin.HandlerFunc {
 			zap.String("user_agent", ctx.Request.UserAgent()),
 			zap.String("method", ctx.Request.Method),
 			zap.String("path", ctx.Request.URL.Path),
-			zap.Int("status", status),
 			zap.String("latency", time.Since(start).String()),
 		}
 
-		if status < http.StatusBadRequest {
-			l.Base().Info("REQUEST SUCCEEDED", fields...)
-		} else {
-			l.Base().Warn("REQUEST FAILED", fields...)
+		errs := ctx.Errors
+		if len(errs) == 0 {
+			fields = append(fields, zap.Int("status", ctx.Writer.Status()))
+
+			l.Base().Info("REQUEST_SUCCEEDED", fields...)
+			return
 		}
+
+		var e *ce.Error
+		if errors.As(errs[0].Err, &e) {
+			fields = append(
+				fields,
+				zap.Int("status", e.ToHTTPStatus()),
+				zap.String("error_code", string(e.Code)),
+				zap.String("error_message", e.Message),
+				zap.String("error_detail", e.Error()),
+			)
+
+			l.Base().Error("REQUEST_FAILED", fields...)
+			utils.SendResponse[any](ctx, e.ToHTTPStatus(), e.Message, nil)
+			return
+		}
+
+		fields = append(
+			fields,
+			zap.Int("status", http.StatusInternalServerError),
+			zap.String("error_code", string(ce.CodeUnknown)),
+			zap.String("error_message", ce.MsgInternalServer),
+			zap.String("error_detail", errs[0].Err.Error()),
+		)
+
+		l.Base().Error("REQUEST_FAILED", fields...)
+		utils.SendResponse[any](ctx, http.StatusInternalServerError, ce.MsgInternalServer, nil)
 	}
 }
